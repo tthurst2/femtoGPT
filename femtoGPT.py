731da609517e4df212407d9b3276a4fdaa3bf9ee
Tyler Thurston <tylerthurston1@gmail.com>
2025-04-17 22:29:05 -0400

    Add train timers, token speed metrics

--- femtoGPT.py
+++ femtoGPT.py
@@ -1,3 +1,4 @@
+import time
 import torch
 import torch.nn as nn
 from torch.nn import functional as F
@@ -275,7 +276,7 @@ torch.manual_seed(1337)
 if torch.cuda.is_available():
     torch.cuda.manual_seed(1337)
 
-train_loader = DataLoader(B=4, T=32)
+train_loader = DataLoader(B=8, T=1024)
 
 model = GPT(GPTConfig())
 model.to(device)
@@ -283,13 +284,19 @@ model.to(device)
 # optimize
 optimizer = torch.optim.AdamW(model.parameters(), lr=3e-4)
 for i in range(50):
+    t0 = time.time()
     x, y = train_loader.next_batch()
     x, y = x.to(device), y.to(device)
     optimizer.zero_grad()
     logits, loss = model(x, y)
     loss.backward()
     optimizer.step()
-    print(f"step {i}, loss: {loss.item()}")
+    torch.cuda.synchronize()
+    t1 = time.time()
+    dt = (t1-t0)*1000  # time diff in ms
+    tokens_per_sec = (train_loader.B * train_loader.T) / (t1 - t0)
+    print(
+        f"step {i}, loss: {loss.item()}, dt: {dt:.2f}ms, tok/sec: {tokens_per_sec}")
 
 print(loss)
 
