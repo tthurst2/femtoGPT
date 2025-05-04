4645808e868fb777f7d732808eb2b6b2883e5eb1
Tyler Thurston <tylerthurston1@gmail.com>
2025-05-04 10:22:37 -0400

    Combine generation code into generate method

--- femtoGPT.py
+++ femtoGPT.py
@@ -422,28 +422,11 @@ if __name__ == "__main__":
             tokens = torch.tensor(tokens, dtype=torch.long)
             tokens = tokens.unsqueeze(0).repeat(num_return_sequences, 1)
             xgen = tokens.to(device)
+            # optionally set seeds
             sample_rng = torch.Generator(device=device)
             sample_rng.manual_seed(42)
-            while xgen.size(1) < max_length:
-                # forward the model to get the logits
-                with torch.no_grad():
-                    logits, loss = model(xgen)  # (B, T, vocab_size)
-                    # take the logits at the last position
-                    logits = logits[:, -1, :]  # (B, vocab_size)
-                    # get the probabilities
-                    probs = F.softmax(logits, dim=-1)
-                    # do top-k sampling of 50 (huggingface pipeline default)
-                    # topk_probs here becomes (5, 50), topk_indices is (5, 50)
-                    topk_probs, topk_indices = torch.topk(probs, 50, dim=-1)
-                    # select a token from the top-k probabilities
-                    # note: multinomial does not demand the input to sum to 1
-                    ix = torch.multinomial(
-                        topk_probs, 1, generator=sample_rng
-                    )  # (B, 1)
-                    # gather the corresponding indices
-                    xcol = torch.gather(topk_indices, -1, ix)  # (B, 1)
-                    # append to the sequence
-                    xgen = torch.cat((xgen, xcol), dim=1)
+            # generate
+            xgen = model.generate(xgen, 32)
             # print the generated text
             for i in range(num_return_sequences):
                 tokens = xgen[i, :max_length].tolist()
